647808a1ce27b42226c788920810d30aa135a683
Bruno Alcântara <brunoalcantarajc@gmail.com>
2020-08-30 19:27:52 -0300

    Adicionado frontend

---
+++ build/precache-manifest.def1eb0cd0fad520593c59bcf0832f9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f38da0b2e5b58e12556579fd4cf2b7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0e0d23a0fdea3c13da5",
+    "url": "/static/css/main.526830b5.chunk.css"
+  },
+  {
+    "revision": "f0d71b604d91189b0947",
+    "url": "/static/js/2.ea9e68b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ea9e68b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e0d23a0fdea3c13da5",
+    "url": "/static/js/main.67473fc6.chunk.js"
+  },
+  {
+    "revision": "7d7ca9f1a06874813fa9",
+    "url": "/static/js/runtime-main.9283f81e.js"
+  },
+  {
+    "revision": "0c9bf7dbed4f3240889a19671873481c",
+    "url": "/static/media/expert.0c9bf7db.jpeg"
+  },
+  {
+    "revision": "62ca5c38b78dbc2c2996e207b6e5e5c3",
+    "url": "/static/media/imgHeader.62ca5c38.jpeg"
+  }
+]);
--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const path = require("path");
 const { errors } = require("celebrate");
 const routes = require("./routes");
 const cors = require("cors");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+app.get("/", function (req, res) {
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  });
+});
 
 app.use(cors());
 app.use(express.json());
